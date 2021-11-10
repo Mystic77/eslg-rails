e3fa6b2245bb048419ac7cb58ce701ae1138eb9d
Jasper <jasper_hendrickx@hotmail.com>
2021-11-10 17:05:07 +0100

    Modify categories styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :categories
   # get 'home/index'
-  root 'home#index'
+  # root 'home#index'
+  root 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
