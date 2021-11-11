63661acecc6c5b38f9f6082b09e7dfc211671115
Jasper <jasper_hendrickx@hotmail.com>
2021-11-11 19:46:48 +0100

    Add React to app

---
+++ app/javascript/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Hello World! App speaking through React now!</div>;
+};
+
+export default App;
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([
+  {
+    name: "Pronunciation",
+    s0: "lorem",
+    s1: "lorem",
+    s2: "lorem",
+    s3: "lorem",
+    s4: "lorem",
+    s5: "lorem",
+    user_id: 2,
+  },
+  {
+    name: "Fluency",
+    s0: "lorem",
+    s1: "lorem",
+    s2: "lorem",
+    s3: "lorem",
+    s4: "lorem",
+    s5: "lorem",
+    user_id: 2,
+  },
+  {
+    name: "Grammar",
+    s0: "lorem",
+    s1: "lorem",
+    s2: "lorem",
+    s3: "lorem",
+    s4: "lorem",
+    s5: "lorem",
+    user_id: 2,
+  },
+  {
+    name: "Vocabulary",
+    s0: "lorem",
+    s1: "lorem",
+    s2: "lorem",
+    s3: "lorem",
+    s4: "lorem",
+    s5: "lorem",
+    user_id: 2,
+  },
+  {
+    name: "Content",
+    s0: "lorem",
+    s1: "lorem",
+    s2: "lorem",
+    s3: "lorem",
+    s4: "lorem",
+    s5: "lorem",
+    user_id: 2,
+  },
+])
